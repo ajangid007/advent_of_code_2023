c3b5cfcb9073891f6b1de1fdc5dfa86989182f16
ajangid007 <anuradha.jangid@gmail.com>
2023-12-24 13:32:05 -0800

    day2

---
+++ day2/part1.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import re
+import collections
+from typing import Dict
+
+class Game:
+    def __init__(self) -> None:
+        self.gameinfo = {}
+
+    def IDSum(self, rmax, gmax, bmax):
+        sum = 0
+        for key, values in self.gameinfo.items():
+            sum += key
+            for r,g,b in values:
+                if not (r <=rmax and g<=gmax and b <=bmax):
+                    sum -= key
+                    break 
+        return sum
+        
+
+    def ReadInput(self, inputfile):
+        with open(os.path.join(sys.path[0], inputfile), "r") as myfile:
+            for line in myfile:
+                id,games  = re.findall(r'Game\s*(\d+)\s*:(.*)', line)[0]
+                self.gameinfo[int(id)] = []
+                for game in games.split(";"):
+                    red,blue,green=0,0,0
+                    for result in re.findall(r'(\d+)\s*(red|green|blue)', game):
+                        red = int(result[0]) if result[1] == 'red' else red
+                        blue = int(result[0]) if result[1] == 'blue' else blue
+                        green = int(result[0]) if result[1] == 'green' else green
+                    self.gameinfo[int(id)].append((red,green, blue))
+        return self
+
+
+
+    
+if __name__== '__main__':
+    sum = Game().ReadInput('input.txt').IDSum(12, 13, 14)
+    print(sum)
